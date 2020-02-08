cddff1ac39aeec7a9edfddaa1065ba23dbc1b462
Bleed1ng <1404134690@qq.com>
2020-02-08 21:38:28 +0800

    lecture materials update

---
+++ src/main/java/com/bleeding/ironbox/IronBoxApplication.java
@@ -0,0 +1,13 @@
+package com.bleeding.ironbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IronBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bleeding/ironbox/IronboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bleeding.ironbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
